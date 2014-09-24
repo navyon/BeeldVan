e60ddbcc61e6c8dd2bfd60a2c0db862ffec080c9
navyon <net_systems@live.com>
2014-09-24 21:35:50 +0200

    latest

--- BeeldVan/BeeldVan/src/org/BvDH/CityTalk/utilities/CompareToSort.java
+++ BeeldVan/BeeldVan/src/org/BvDH/CityTalk/utilities/CompareToSort.java
@@ -10,9 +10,8 @@ public class CompareToSort implements Comparator<Locations>
 		public int compare(Locations l1, Locations l2)
 			{
 
-				String distance1 = l1.getName();
-				String distance2 = l2.getName();
 
-				return distance1.compareTo(distance2);
+                return (int) (l1.getDistance()-l2.getDistance());
+
 			}
 	}
